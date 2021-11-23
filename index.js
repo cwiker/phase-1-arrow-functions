f20931df31ce850dc1be5ebfc5447fc90f3e3e23
Chris Wiker <cwiker7@gmail.com>
2021-11-23 11:49:55 -0500

    commit tree 828fb7f8f2e50a0c35f710d50a9447f4b0342b36

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide() {
+    return 2000 / 100;
+}
+
+const square = parameter1 => parameter1 * parameter1;
+square(3,3);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
